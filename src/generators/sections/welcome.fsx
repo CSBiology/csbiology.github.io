f712b2f2808029cfde5a8696a25c681f0d64d1f8
Kevin F <Freymaurer@gmx.de>
2023-07-03 21:06:28 +0200

    try fix production

--- src/generators/sections/welcome.fsx
+++ src/generators/sections/welcome.fsx
@@ -9,7 +9,7 @@ let generate (ctx : SiteContents) (_: string) =
             p [] [ !! "The capability of biological systems to respond to environmental changes is realized by a complex dynamic adjustment of the interplay between genes, proteins and metabolites. For a deeper understanding at the systems level, we need to study the structure and dynamics of cellular and organismal functions rather than the characteristics of isolated parts of a cell or an organism."];
         ];
         div [Class "welcome__overlay"] [];
-        video [] [
+        video [ AutoPlay true; Loop true; Class "welcome__video"; Muted true ] [
             source [Src "./style/video/mp4/Working-Space.mp4"; Type "video/mp4"];
             source [Src "./style/video/ogv/Working-Space.ogv"; Type "video/mp4"];
             source [Src "./style/video/webm/Working-Space.webm"; Type "video/webm"];
